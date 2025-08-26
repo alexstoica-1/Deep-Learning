960f9cb0e23ef1a40f9c75d930fb2ab485d15e27
alexisgod <alexandru.stoica2004@yahoo.com>
2025-08-26 15:01:05 +0300

    Added a CNN

--- neural_network/networks.py
+++ neural_network/networks.py
@@ -28,6 +28,24 @@ TEST:
     print(model(x).shape) # torch.Size([64, 10])
 """
 
+class CNN(nn.Module):
+    def __init__(self, in_channels = 1, num_classes = 10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3,3), stride=(1,1), padding=(1,1)) # same convolution with this values 
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)) # half the dimension size
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1)) 
+        self.fc1 = nn.Linear(16*7*7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -37,6 +55,7 @@ num_classes = 10
 learning_rate = 0.001
 batch_size = 64
 num_epochs = 1
+num_epochs_cnn = 5
 
 # Load Data
 train_dataset = datasets.MNIST(root = 'neural_network/dataset/', train = True, transform = transforms.ToTensor(), download = True)
@@ -45,14 +64,16 @@ train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuf
 test_dataset = datasets.MNIST(root = 'neural_network/dataset/', train = False, transform = transforms.ToTensor(), download = True)
 test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = True)
 
-# Network
+# Networks
 model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model_cnn = CNN().to(device)
 
 # Loss function
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr =learning_rate)
+optimizer_cnn = optim.Adam(model_cnn.parameters(), lr =learning_rate)
 
-# Train Network
+# Train FeedForward Network
 
 for epoch in range(num_epochs):
     epoch_start = time.time()
@@ -104,3 +125,52 @@ def check_accuracy(loader, model):
 
 check_accuracy(train_loader, model)
 check_accuracy(test_loader, model)
+
+# Train Convolutional Network
+
+for epoch in range(num_epochs_cnn):
+    epoch_start = time.time()
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        data = data.to(device = device)
+        targets = targets.to(device = device)
+
+        # Forward
+        scores = model_cnn(data)
+        loss = criterion(scores, targets) 
+
+        # Backward
+        optimizer_cnn.zero_grad() # set all the gradients to 0 for each batch 
+        loss.backward()
+
+        # Gradient Descent or Adam step
+        optimizer_cnn.step()
+
+    epoch_end = time.time()
+    print(f"Epoch {epoch+1} for CNN took {epoch_end - epoch_start:.2f} seconds.")
+
+def check_accuracy_cnn(loader, model):
+    if loader.dataset.train:
+        print("Checking accuracy on training data:")
+    else:
+        print("Checking accuracy on test data:")
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x,y in loader:
+            x = x.to(device = device)
+            y = y.to(device = device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+        
+        print(f"Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples)*100:.2f}.")
+
+    model.train()
+
+check_accuracy_cnn(train_loader, model_cnn)
+check_accuracy_cnn(test_loader, model_cnn)
